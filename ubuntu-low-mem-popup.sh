68f9377c5971bbd04a5740e72596baf8270f2941
Roman Melko <romanmelko@gmail.com>
2019-03-06 15:12:39 +0200

    initial commit

---
+++ ubuntu-low-mem-popup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+THRESHOLD=500
+INTERVAL=300
+POPUP_DELAY=999999
+
+# sleep some time so the shell starts properly
+sleep 60
+
+while :
+do
+    available=$(free -m | awk '/^Mem:/{print $7}')
+    if [ $available -lt $THRESHOLD ]; then
+        title="Low memory! $available MB available"
+        message=$(top -bo %MEM -n 1 | grep -A 3 PID | awk '{print $(NF - 6) " \t" $(NF)}')
+        # KDE Plasma notifier
+        kdialog --title "$title" --passivepopup "$message" $POPUP_DELAY
+        # use the following command if you are not using KDE Plasma, comment the line above and uncomment the line below
+        # please note that timeout for notify-send is represented in milliseconds
+        # notify-send "$title" "$message" -t $POPUP_DELAY
+    fi
+    sleep $INTERVAL
+done
